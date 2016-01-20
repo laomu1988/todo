23cd6c55d0e7f8b19cd97324024c501caa83fff1
muzhilong <laomu1988@qq.com>
2016-01-20 20:58:37 +0800

    正在加载状态；当前菜单；

--- src/js/common/config.js
+++ src/js/common/config.js
@@ -3,7 +3,7 @@ var config = web.config = {
     onAjaxload: function (result) {
         if (result && result.code == 401) {
             var login = $('[riot-tag=login]');
-            if (login) {
+            if (login && login.length > 0) {
                 login.find('.modal').modal();
             } else {
                 web.mount('login');
--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,40 @@
 $(function () {
+    var loading = $('.loading');
+
+    function showList(result, msg) {
+        loading.hide();
+        msg = msg || '加载失败！';
+        if (result && result.code == 0) {
+            riot.mount('todo_list', result.data);
+        }
+        else {
+            web.message((result && result.message) || msg);
+        }
+    }
+
     function route() {
         var method = web._hashs.method;
+        loading.show();
         switch (method) {
             case 'project':
                 web.services.todo.list({project: web._hashs.project}, function (result) {
-                    if (result && result.code == 0) {
-                        riot.mount('todo_list', result.data);
-                    }
+                    showList(result, '加载失败！');
                 });
                 break;
             case 'finished':
                 web.services.todo.list({finished: true}, function (result) {
-                    if (result && result.code == 0) {
-                        riot.mount('todo_list', result.data);
-                    }
+                    showList(result, '加载失败！');
                 });
                 break;
             case 'removed':
                 web.services.todo.list({removed: true}, function (result) {
-                    if (result && result.code == 0) {
-                        riot.mount('todo_list', result.data);
-                    }
+                    showList(result, '加载失败！');
                 });
                 break;
             case 'todo':
             default:
                 web.services.todo.list(function (result) {
-                    if (result && result.code == 0) {
-                        riot.mount('todo_list', result.data);
-                    }
+                    showList(result, '加载失败！');
                 });
                 break;
         }
